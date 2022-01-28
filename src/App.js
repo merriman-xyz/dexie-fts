83efabc1359434ad3b1eda8b86679c284ecdad42
b <b@merriman.xyz>
2022-01-28 08:15:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {
+    populateDatabase,
+    query_naiveFilterToArray,
+    query_naiveToArrayFilter,
+    query_rawTokenSearch, query_soundexTokenSearch, query_stemmedSoundexTokenSearch, query_stemmedTokenSearch,
+    runFTSTest
+} from "./databaseFTS";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
+const ResultTable = ({query, results}) => {
+    return <div>
+        <h2>{query}</h2>
+        <table>
+            <thead>
+            <th>Query Type</th>
+            <th>Query Time (ms)</th>
+            <th>Query Result Count</th>
+            </thead>
+            <tbody>
+            {results.map((result) => <tr key={result.type}>
+                <td>{result.type}</td>
+                <td>{result.query_time} ms</td>
+                <td>{result.queryResults_length}</td>
+            </tr>)}
+            </tbody>
+        </table>
+    </div>
+}
+
+const queryTypes = [
+    'NAIVE-FILTER-TO-ARRAY',
+    'NAIVE-TO-ARRAY-FILTER',
+    'RAW-TOKEN',
+    'STEMMED-TOKEN',
+    'SOUNDEX-TOKEN',
+    'STEMMED-SOUNDEX-TOKEN'
+]
+
+const QueryByType = ({queryType, setQueryType}) => {
+    return <select value={queryType} onChange={(e) => setQueryType(e.target.value)} name="" id="">
+        {queryTypes.map(qt => <option key={qt} value={qt}>{qt}</option>)}
+    </select>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [paragraphs, setParagraphs] = useState([])
+    const [progress, setProgress] = useState('')
+    const [query, setQuery] = useState('houses')
+    const [liveQuery, setLiveQuery] = useState('')
+    const [liveQueryType, setLiveQueryType] = useState(queryTypes[0])
+    const [liveQueryTime, setLiveQueryTime] = useState(0)
+    const [liveQueryResults, setLiveQueryResults] = useState([])
+    const [liveQueryProcessed, setLiveQueryProcessed] = useState('')
+    const [results, setResults] = useState([])
+    const [inputQuery, setInputQuery] = useState('')
+
+    useEffect(() => {
+        const aiw = "https://gist.githubusercontent.com/phillipj/4944029/raw/75ba2243dd5ec2875f629bf5d79f6c1e4b5a8b46/alice_in_wonderland.txt"
+        const wap = "https://raw.githubusercontent.com/mmcky/nyu-econ-370/master/notebooks/data/book-war-and-peace.txt"
+        populateDatabase(aiw, setProgress).then(paragraphs$processed => {
+            setParagraphs(paragraphs$processed)
+            return runFTSTest(query)
+        }).then(results => {
+            setResults(results)
+        })
+
+    }, [])
+
+    useEffect(() => {
+        if (paragraphs.length === 0) {
+            return
+        }
+        setResults([])
+        runFTSTest(query).then(results => {
+            setResults(results)
+        })
+    }, [query])
+
+    useEffect(() => {
+        if (liveQuery.trim() === '') return
+    }, [liveQuery])
+
+    const runLiveQuery = async (e) => {
+
+        e.preventDefault()
+        const query = e.target.value
+        setLiveQuery(query)
+        if (e.target.value.trim() === '') return
+        let result
+
+        switch (liveQueryType) {
+            case 'NAIVE-FILTER-TO-ARRAY':
+                result = await query_naiveFilterToArray(query);
+                break;
+            case 'NAIVE-TO-ARRAY-FILTER':
+                result = await query_naiveToArrayFilter(query);
+                break;
+            case 'RAW-TOKEN':
+                result = await query_rawTokenSearch(query);
+                break;
+            case 'STEMMED-TOKEN':
+                result = await query_stemmedTokenSearch(query);
+                break;
+            case 'SOUNDEX-TOKEN':
+                result = await query_soundexTokenSearch(query);
+                break;
+            case 'STEMMED-SOUNDEX-TOKEN':
+                result = await query_stemmedSoundexTokenSearch(query);
+                break;
+            default:
+                return
+        }
+
+        setLiveQueryTime(result.query_time)
+        setLiveQueryResults(result.queryResults)
+        setLiveQueryProcessed(result.queryProcessed.join(', '))
+
+    }
+
+    return (
+        <div>
+            <header>
+                <h1>Fulltext Search Experimentation</h1>
+                <nav>
+                    <Link to={'test'}>Testing</Link>
+                    <Link to={'liveQuery'}>Live Querying</Link>
+                </nav>
+            </header>
+            <input value={inputQuery} onChange={e => setInputQuery(e.target.value)} type="text"/>
+            <button onClick={() => setQuery(inputQuery)}>Input Query</button>
+            <ResultTable query={query} results={results}></ResultTable>
+            <form action="">
+                <input type="text" value={liveQuery} onChange={e => runLiveQuery(e)}/>
+                <QueryByType queryType={liveQueryType} setQueryType={setLiveQueryType}/>
+                {liveQueryTime}
+                {liveQueryProcessed}
+                {liveQueryResults.map(p => <p key={p.id}>{p.text}</p>)}
+            </form>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/databaseFTS.js
@@ -0,0 +1,252 @@
+import Dexie from "dexie";
+import _ from "lodash";
+import {soundex} from "soundex-code";
+import {stemmer} from "stemmer";
+import {nanoid} from "nanoid";
+import {queryByText} from "@testing-library/react";
+
+const database = new Dexie("FTS-FULL-TEST");
+database.version(1).stores({
+    paragraphs: '++id, text, *tokens, *tokens_stemmed, *tokens_soundex, *tokens_stemmedSoundex'
+});
+
+const splitNormalizeText = (text) => text
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9 ]/gi, ' ')
+    .split(/\s+/)
+    .filter(word => word.trim() !== '')
+    .map(word => _.deburr(word))
+
+const resetDatabase = async () => {
+    await database.paragraphs.toCollection().delete()
+}
+
+export const populateDatabase = async (srcURL, setProgress) => {
+
+    setProgress('RESETTING DATABASE...')
+    await resetDatabase();
+
+    setProgress('FETCHING SOURCE...')
+    const response = await fetch(srcURL)
+    const text = await response.text()
+    const paragraphs = text.split('\n\n')
+    let paragraphs$processed = []
+
+    setProgress('PROCESSING SOURCE PARAGRAPHS...')
+
+    try {
+        paragraphs$processed = paragraphs.map(paragraph_text => {
+
+            const text = paragraph_text.trim()
+
+            const tokens = [...new Set(text
+                .toLowerCase()
+                .replace(/[^a-z0-9 ]/gi, ' ')
+                .split(/\s+/)
+                .filter(word => word.trim() !== '')
+                .map(word => _.deburr(word)))
+                .values()]
+
+            const tokens_soundex = tokens.map(t => soundex(t))
+            const tokens_stemmed = tokens.map(t => stemmer(t))
+            const tokens_stemmedSoundex = tokens_stemmed.map(t => soundex(t))
+
+            // const p = document.createElement('p')
+            // p.innerHTML = `${text} <br><br> ${tokens.join(', ')}`
+            // document.body.append(p)
+
+            return {
+                id: nanoid(), text, tokens, tokens_stemmed, tokens_soundex, tokens_stemmedSoundex
+            }
+
+        })
+
+        setProgress('BULK ADDING SOURCE PARAGRAPHS TO DATABASE...')
+        await database.paragraphs.bulkAdd(paragraphs$processed)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    setProgress('FETCHING AND PROCESSING COMPLETE')
+    return paragraphs$processed
+
+}
+
+export const query_naiveFilterToArray = async (queryText) => {
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const start = performance.now()
+    let queryResults
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = await database.paragraphs.filter(paragraph => new RegExp(`\\b${normalizedSplitQuery[0]}\\b`, 'gi').test(paragraph.text)).toArray()
+            break
+        default:
+            queryResults = await database.paragraphs.filter(paragraph => new RegExp(`\\b${normalizedSplitQuery.join('|')}\\b`, 'gi').test(paragraph.text)).toArray()
+            break
+    }
+
+    return {
+        type: 'NAIVE - FILTER THEN TO ARRAY',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+}
+
+export const query_naiveToArrayFilter = async (queryText) => {
+    // queryText = normalizeText(queryText)
+
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const start = performance.now()
+    let queryResults
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = (await database.paragraphs.toArray()).filter(paragraph => new RegExp(`\\b${normalizedSplitQuery[0]}\\b`, 'gi').test(paragraph.text))
+            break
+        default:
+            queryResults = (await database.paragraphs.toArray()).filter(paragraph => new RegExp(`\\b${normalizedSplitQuery.join('|')}\\b`, 'gi').test(paragraph.text))
+            break
+    }
+
+    return {
+        type: 'NAIVE - TO ARRAY THEN FILTER',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+}
+
+export const query_rawTokenSearch = async (queryText) => {
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const start = performance.now()
+    let queryResults
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = await database.paragraphs.where('tokens').equals(normalizedSplitQuery[0]).toArray()
+            break
+        default:
+            queryResults = await database.paragraphs.where('tokens').anyOf(normalizedSplitQuery).toArray()
+            break
+    }
+
+    return {
+        type: 'RAW TOKEN SEARCH',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+
+}
+
+export const query_stemmedTokenSearch = async (queryText, AND) => {
+
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const normalizedSplitQuery$stemmed = normalizedSplitQuery.map(token => stemmer(token))
+    const start = performance.now()
+    let queryResults
+
+    console.log(normalizedSplitQuery$stemmed)
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = await database.paragraphs.where('tokens_stemmed').equals(normalizedSplitQuery$stemmed[0]).toArray()
+            break
+        default:
+            if(AND){
+
+            } else {
+                queryResults = await database.paragraphs.where('tokens_stemmed').anyOf(normalizedSplitQuery$stemmed).toArray()
+            }
+            break
+    }
+
+    return {
+        type: 'STEMMED TOKEN SEARCH',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+
+}
+
+export const query_soundexTokenSearch = async (queryText) => {
+
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const normalizedSplitQuery$soundex = normalizedSplitQuery.map(token => soundex(token))
+    const start = performance.now()
+    let queryResults
+
+    console.log(normalizedSplitQuery$soundex)
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = await database.paragraphs.where('tokens_soundex').equals(normalizedSplitQuery$soundex[0]).toArray()
+            break
+        default:
+            queryResults = await database.paragraphs.where('tokens_soundex').anyOf(normalizedSplitQuery$soundex).toArray()
+            break
+    }
+
+    return {
+        type: 'SOUNDEX TOKEN SEARCH',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+
+}
+
+export const query_stemmedSoundexTokenSearch = async (queryText) => {
+
+    const normalizedSplitQuery = splitNormalizeText(queryText)
+    const normalizedSplitQuery$soundex = normalizedSplitQuery.map(token => soundex(stemmer(token)))
+    const start = performance.now()
+    let queryResults
+
+    console.log(normalizedSplitQuery$soundex)
+    switch (normalizedSplitQuery.length) {
+        case 0: return null
+        case 1:
+            queryResults = await database.paragraphs.where('tokens_stemmedSoundex').equals(normalizedSplitQuery$soundex[0]).toArray()
+            break
+        default:
+            queryResults = await database.paragraphs.where('tokens_stemmedSoundex').anyOf(normalizedSplitQuery$soundex).toArray()
+            break
+    }
+
+    return {
+        type: 'STEMMED SOUNDEX TOKEN SEARCH',
+        query_time: performance.now() - start,
+        queryProcessed: normalizedSplitQuery,
+        queryResults,
+        queryResults_length: queryResults.length
+    }
+
+}
+
+export const runFTSTest = async (queryText) => {
+
+    const results = []
+
+    await new Promise(resolve => setTimeout(resolve, 1)) // clear
+
+    results.push(await query_naiveToArrayFilter(queryText))
+    results.push(await query_naiveFilterToArray(queryText))
+    results.push(await query_rawTokenSearch(queryText))
+    results.push(await query_stemmedTokenSearch(queryText))
+    results.push(await query_soundexTokenSearch(queryText))
+    results.push(await query_stemmedSoundexTokenSearch(queryText))
+
+    return results
+
+}
